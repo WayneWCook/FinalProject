e6f19baa7369b6993435678a3ef4c87c507c695e
LukeLangius <lukelangius@gmail.com>
2020-05-08 18:14:07 -0600

    Changed a lot on the story time class. I added a try catch for everything, added additional comments, and modified the header comments. :)

--- src/Storytime.java
+++ src/Storytime.java
@@ -1,9 +1,8 @@
 /*Storytime.java
-Alessandro Krapf, Luke Langius, and Safwan Ahmad
-5.6.2020
-java 160
-This program runs a game. If you get it correct you move on until you beat the game, and if you get it wrong it restarts.
-It uses classes hardchoices and thePlot.
+ *Luke Langius, Alessandro Krapf, and Safwan Ahmad
+ * 5/8/2020
+ * Java 160
+ * This program runs a game. If you get it correct you move on until you beat the game, and if you get it wrong it restarts. It uses classes hardchoices and thePlot.
  */
 import java.util.Scanner;
 public class Storytime {
@@ -11,26 +10,71 @@ public class Storytime {
         Scanner input = new Scanner(System.in);
         hardchoices x = new hardchoices();
         thePlot s = new thePlot();
-        String choice;
+        int userChoice = 0;
         boolean loop  = false;
+        boolean tryCatch = false;
         System.out.println("What is your name?");
         s.name = input.nextLine();
         do{    //if the user loses, the program will restart from this point.
+            lable1:           //The code uses "lable1" later to break out of a multiple loops at once
             for (int a=1; a<=13; a++) {   //for loop runs all of the story.
                 s.storyX = a;   //decides which story to display by using the number of the for loop.
                 System.out.println(s.Stories());    //displays story.
-                if (s.storyX ==1) { choice = input.nextLine();
-                    if (!choice.equals("Y")) System.out.println("It does not matter.");}
-                else if (s.storyX ==2) s.politeVariable = input.nextLine();    //since the 1st, 2nd, and 13th parts of the Story have diff necessities.
-                else if (s.storyX > 2 && s.storyX < 13) {
-                    choice = input.nextLine();
-                    x.loop2 = choice.equals(s.correct());   //makes boolean true if the user input is correct.
-                    System.out.print(x.incorrect());   //if user input incorrect, displays text saying so.
-                    if (x.incorrect().equals(x.wrongChoice)) {  //if text saying user was wrong is displayed, then the program will restart.
-                        loop = true;    //allows program to restart
-                        break;                       //exits for loop program
+                do {
+                    try {                     //start of try catch
+                        if (s.storyX == 1) {                                                                       //doing the try catch for the first part of the story
+                            userChoice = input.nextInt();
+                            if (userChoice < 1 || userChoice > 2) {
+                                throw new ArithmeticException("Please type in either 1 or 2: ");                 //throwing an exception if the user doesn't enter 1 or 2
+                            }
+                            tryCatch = false;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Please type in either 1 or 2: ");                       //catching another exception if the user doesn't enter 1 or 2, and asks them to enter 1 or 2
+                        input.nextLine();
+                        tryCatch = true;
                     }
-                } else if (s.storyX == 13) loop = false;   //if the script finishes, the program ends itself.
+                } while (tryCatch);                         //end of try and catch
+                if (s.storyX == 1) { if (userChoice != 1) {System.out.println("That's unfortunate for you, you don't have a choice");}}         //this makes it so no matter what you choose for the first part, you will always go through into the next room
+                do {
+                    try {
+                        if (s.storyX == 2) {                        //try catch for the second part of the story
+                            userChoice = input.nextInt();
+                            if (userChoice == 3) {s.politeVariable = userChoice;}            //there is an option for the 3rd part to use your name, so if the user chooses 3, there name will be saved as "politeVariable"
+                            if (userChoice < 1 || userChoice > 3) {
+                                throw new ArithmeticException("Please type in a number 1 - 3: ");       //throwing the exception
+                            }
+                            tryCatch = false;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Please type in a number 1 - 3: ");                   //catching the exception and asking the user to type in a number from 1 - 3
+                        input.nextLine();
+                        tryCatch = true;
+                    }
+                } while (tryCatch);                      //end of try and catch
+                do {
+                    try {
+                        if (s.storyX > 2 && s.storyX < 13) {                  //doing the try catch for the rest of the story (parts 3 - 12)
+                            userChoice = input.nextInt();
+                            if (userChoice < 1 || userChoice > 3) {
+                                throw new ArithmeticException("Please type in a number 1 - 3: ");       //throws exception if the user don't enter a number in the right parameters, or the user enters anything besides a number
+                            }
+                            x.loop2 = userChoice == (s.correct());                                 //checking if the user entered the correct answer
+                            if (x.incorrect().equals(x.wrongChoice)) {
+                                loop = true;                               //if the user entered the incorrect answer, they would be reset back to the very beginning of the game.
+                                break lable1;               //since this if statement is inside of a do while loop and a for loop, I set a lable1 before the for loop so that if the user enters the incorrect answer it would break out of both of the loops and restart the game
+                            } else if (s.storyX == 13) {
+                                loop = false;                         //if the user got all of the correct answer it will break them out of the do while loop and allow them to win the game
+                                break lable1;
+                            }
+                            tryCatch = false;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Please type in a number 1 - 3: ");         //catching the thrown exception
+                        input.nextLine();
+                        tryCatch = true;
+                    }
+                } while (tryCatch);                  //end of try and catch
             }
         } while (loop);
     }
--- src/thePlot.java
+++ src/thePlot.java
@@ -1,84 +1,87 @@
 /*thePlot.java
-Safwan Ahmad, Alessandro Krapf, and Luke Langius
-5.5.2020
-java 160
-This program displays all of the story. It also defines the right choices for each option of the game.
-It works for the main class.
+ *Safwan Ahmad, Alessandro Krapf, and Luke Langius
+ * 5/8/2020
+ * Java 160
+ * This program displays all of the story. It also defines the right choices for each option of the game. It works for the main class.
  */
 public class thePlot {
     public int storyX;
-    public String name, politeVariable;
+    public String name;
+    public int politeVariable;
     public String Polite() {    //If in Story part 2 the user decides to give name, then will display name in Part 3.
         String Polite;
-        if (politeVariable.equals("C")) Polite = name;
-        else Polite = "no matter who you are";
+        if (politeVariable == 3) Polite = name;
+        else Polite = "no matter who you are"; // if user decides not to give name, it will just display "no matter who you are"
         return Polite;
     }
     public String Stories() {           //Is story. Will print the story in correct order because of for loop in main.
         if (storyX == 1) return "Your character rides in on a horse into an abandoned ghost town. You step into a building to check for supplies. " +
                 "\nYou look in and around the abandoned saloon. You see a dark room.\n" +
-                "Do you want to continue to the room(Y/N)?";
+                "Do you want to continue to the room?\n" +
+                "1. Yes\n" +
+                "2. No\n" +
+                "Choice: ";
         else if (storyX == 2) return "You continue to the room. You hear a large rumbling surrounding the room. A large green light is going in the walls. " +
                 "\nYou hear a loud voice coming from the walls. A green ball comes from the wall like an egg. \nIt hatches and there it stands a large half rotting corpse standing in front of you. \n" +
                 "\"Hello there, now whats your name?\"\n" +
                 "Choices:\n" +
-                "A. I don’t have to answer that\n" +
-                "B.Rude gesture\n" +
-                "C.Your character name";
+                "1. I don’t have to answer that\n" +
+                "2. Rude gesture\n" +
+                "3. Your character name";
         else if (storyX == 3) return "Well, " + Polite() + ", you ain’t leaving this place alive. And when you get to hell tell ‘em Johnny Trainwreck sent you!”\n" +
                 "Johnny Trainwreck’s first attack will be a train throw.\n" +
-                "You will have to pick the correct answer from 3 letters as your choices. Use only capitals.\n" +
-                "A. Dodge the train and fire\n" +
-                "B. Try to pass through the train and fire\n" +
-                "C. Lay down and die";
+                "You will have to pick the correct answer from 3 numbers as your choices. Use only numbers.\n" +
+                "1. Dodge the train and fire\n" +
+                "2. Try to pass through the train and fire\n" +
+                "3. Lay down and die";
         else if (storyX == 4) return "\"ARGH, well you got lucky. See if you can handle this next attack.\"\n" +
                 "Next attack is a giant rail spike rain which follows you.\n" +
-                "A. Run and dodge the spikes which follow, you a futile task\n" +
-                "B. Take a spike while guarding most of your body, then fire\n" +
-                "C. Lay down and die";
+                "1. Run and dodge the spikes which follow, you a futile task\n" +
+                "2. Take a spike while guarding most of your body, then fire\n" +
+                "3. Lay down and die";
         else if (storyX == 5) return "\"That seems weirdly lucky, like something is helping you.\"\n" +
                 "The final attack is a giant railway maze attack.\n" +
-                "A.Let the railway pierce you with everything\n" +
-                "B. Lay down and die\n" +
-                "C.Hide behind a table and then shoot him";
+                "1. Let the railway pierce you with everything\n" +
+                "2. Lay down and die\n" +
+                "3. Hide behind a table and then shoot him";
         else if (storyX == 6) return "\"ARRRRRRGGHH!\" and Johnny falls over, and dies.\n" +
                 "You will then look around and move to the next room\n" +
                 "Then you will hear two cackling voices,and two more green balls will come from the wall.\n" +
                 "“We are the TWINS, yes that is what our mother named us. Don’t judge\uD83D\uDE1D\n" +
                 "First attack will be two hammers attacking you at once.\n" +
-                "A. Lay down and die\n" +
-                "B. Take one in the face and shoot them both\n" +
-                "C. Try and dodge";
+                "1. Lay down and die\n" +
+                "2. Take one in the face and shoot them both\n" +
+                "3. Try and dodge";
         else if (storyX == 7) return "\"UGH, your a harder kill than we thought.\" " +
                 "Two whips are being whipped at you.\n" +
-                "A. Take a whipping like the loser you are\n" +
-                "B. Catch both whips and shoot both\n" +
-                "C. Lay down and die";
+                "1. Take a whipping like the loser you are\n" +
+                "2. Catch both whips and shoot both\n" +
+                "3. Lay down and die";
         else if (storyX == 8) return "Their final attack will be high pitched screaming.\n" +
-                "A. SHUT UP AND KILL THEM BOTH\n" +
-                "B.  Run away and never look back\n" +
-                "C. Lay down and die while your head hurts";
+                "1. SHUT UP AND KILL THEM BOTH\n" +
+                "2. Run away and never look back\n" +
+                "3. Lay down and die while your head hurts";
         else if (storyX == 9) return "The Twins explode and they slink back to whatever place they need to be.\n" +
                 "You hear a dark rumbling and a man  in western dark garb approaches. You hear him whisper something you can't understand. Then silence.\n" +
                 "You realize that this \"man\", is not a man. He is 10 feet tall,  and you understand nothing. Even your own thoughts are turning to gibberish.\n" +
                 "Your sight is becoming blurred. This Thing is in your head.\n" +
                 "The last thing you realize is that you will have to guess what you want to do, since you don't know what you will be doing anymore.\n" +
                 "1.@$#%^YTVCJFDRT\n" +
-                "A. Ymg''ll ah'mglw'nafh\n" +
-                "B. Ymg''ll ah'mglw'nafh\n" +
-                "C. Ymg''ll ah'mglw'nafh";
+                "1. Ymg''ll ah'mglw'nafh\n" +
+                "2. Ymg''ll ah'mglw'nafh\n" +
+                "3. Ymg''ll ah'mglw'nafh";
         else if (storyX == 10) return "2.@#$%^&*(&^%$##&*&*\n" +
-                "A. Ahh ymg' ah mgephai geb\n" +
-                "B. Ahh ymg' ah mgephai geb\n" +
-                "C. Ahh ymg' ah mgephai geb";
+                "1. Ahh ymg' ah mgephai geb\n" +
+                "2. Ahh ymg' ah mgephai geb\n" +
+                "3. Ahh ymg' ah mgephai geb";
         else if (storyX == 11) return "3.$%^&UYGTFYR$^%&^&*(\n" +
-                "A. Zhro ya\n" +
+                "1. Zhro ya\n" +
                 "B. Zhro ya\n" +
                 "C. Zhro ya";
         else if (storyX == 12) return "4.$%^&*(UIYUGTFR^$\n" +
-                "A. Fahf ah weird nafl l' bug ah'legeth\n" +
-                "B. Fahf ah weird nafl l' bug ah'legeth\n" +
-                "C. Fahf ah weird nafl l' bug ah'legeth";
+                "1. Fahf ah weird nafl l' bug ah'legeth\n" +
+                "2. Fahf ah weird nafl l' bug ah'legeth\n" +
+                "3. Fahf ah weird nafl l' bug ah'legeth";
         else if (storyX == 13) return "\"You see black everywhere, yet you somehow see the black being evaporate. Then nothing.\"\n" +
                 "You don't feel anything. However, you her a voice talking to you:\n" +
                 "\"Hello. You died. I have existed forever.\n" +
@@ -87,18 +90,18 @@ public class thePlot {
                 "Congratulations. For your feat, I have ended the loop. I will allow you to experience death now.\"";
         else return "";
     }
-    public String correct(){
-        String correct="other";
-        if (storyX==3) correct = "A";
-        else if (storyX == 4) correct = "B";  //defines what the correct answer for each question is.
-        else if (storyX == 5) correct = "C";
-        else if (storyX == 6) correct = "B";
-        else if (storyX == 7) correct = "B";
-        else if (storyX == 8) correct = "A";
-        else if (storyX ==9) correct = "B";
-        else if (storyX ==10) correct = "A";
-        else if (storyX ==11) correct = "C";
-        else if (storyX ==12) correct = "C";
+    public int correct(){
+        int correct = 0;
+        if (storyX==3) correct = 1;
+        else if (storyX == 4) correct = 2;  //defines what the correct answer for each question is.
+        else if (storyX == 5) correct = 3;
+        else if (storyX == 6) correct = 2;
+        else if (storyX == 7) correct = 2;
+        else if (storyX == 8) correct = 1;
+        else if (storyX == 9) correct = 2;
+        else if (storyX ==10) correct = 1;
+        else if (storyX ==11) correct = 3;
+        else if (storyX ==12) correct = 3;
         return correct;
     }
 }
